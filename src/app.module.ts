4b05a73e6245bad2deb0bda069010dad0abfa09b
Andrii Pereverziev <pereverziev.andrii@gmail.com>
2020-12-03 20:02:21 +0200

    .env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+    }),
     ProductsModule,
-    MongooseModule.forRoot(``)
+    MongooseModule.forRoot(process.env.DB_URI)
   ],
   controllers: [AppController],
   providers: [AppService],
